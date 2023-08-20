8f087dd405960d20509b5069cf25c085e0c97743
Christiaan Bloemendaal <github@tnrd.net>
2023-08-20 21:19:22 +0200

    refactor: changed to use Seq

--- Zeepkist.WorkshopApi.Drone/LogtailOptions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TNRD.Zeepkist.WorkshopApi.Drone;
-
-public class LogtailOptions
-{
-    public string Format { get; set; } = null!;
-    public string SourceToken { get; set; } = null!;
-}
--- Zeepkist.WorkshopApi.Drone/Program.cs
+++ Zeepkist.WorkshopApi.Drone/Program.cs
@@ -1,22 +1,19 @@
-using Logtail.NLog;
-using Microsoft.Extensions.Options;
-using NLog;
-using NLog.Config;
 using Serilog;
 using TNRD.Zeepkist.WorkshopApi.Drone;
 using TNRD.Zeepkist.WorkshopApi.Drone.Api;
 using TNRD.Zeepkist.WorkshopApi.Drone.Google;
 using TNRD.Zeepkist.WorkshopApi.Drone.Steam;
-using LogLevel = NLog.LogLevel;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .UseConsoleLifetime()
     .UseSerilog((context, configuration) =>
     {
         configuration
-            .WriteTo.Console()
-            .WriteTo.NLog()
-            .MinimumLevel.Debug();
+            .Enrich.FromLogContext()
+            .Enrich.WithProperty("Source", "Zworpshop")
+            .MinimumLevel.Debug()
+            .WriteTo.Seq(context.Configuration["Seq:Url"], apiKey: context.Configuration["Seq:Key"])
+            .WriteTo.Console();
     })
     .ConfigureServices((context, services) =>
     {
@@ -30,22 +27,7 @@ IHost host = Host.CreateDefaultBuilder(args)
 
         services.Configure<SteamOptions>(context.Configuration.GetSection("Steam"));
         services.AddHttpClient<SteamClient>();
-
-        services.Configure<LogtailOptions>(context.Configuration.GetSection("Logtail"));
     })
     .Build();
-
-LogtailOptions logtailOptions = host.Services.GetRequiredService<IOptions<LogtailOptions>>().Value;
-
-LoggingConfiguration config = new();
-LogtailTarget logtailTarget = new()
-{
-    Name = "logtail",
-    Layout = logtailOptions.Format,
-    SourceToken = logtailOptions.SourceToken
-};
-config.AddRule(LogLevel.Trace, LogLevel.Fatal, logtailTarget, "*");
-
-LogManager.Configuration = config;
 
 host.Run();
